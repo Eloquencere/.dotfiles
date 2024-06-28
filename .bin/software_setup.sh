1b60307a0850a6858cfaa9cfbe98c65816c7be0d
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-06-28 23:50:46 +0530

    Update software_setup.sh

--- .bin/software_setup.sh
+++ .bin/software_setup.sh
@@ -4,6 +4,8 @@ cd ~/Documents/install_script_temp_folder
 # Basic setup
 sudo python ~/.dotfiles/.bin/conf_grub.py # removing grub screen on startup
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo touch /etc/modules-load.d/usbip.conf # adding basic conf to usbip 
+sudo sh -c "printf '%s\n%s\n' 'usbip-core' 'vhci-hcd' >> /etc/modules-load.d/usbip.conf"
 sudo python ~/.dotfiles/.bin/conf_pacman.py
 
 # Temporary setup for zsh shell
