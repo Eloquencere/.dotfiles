0b444a0a93e331d77128e3c552b1b19184b78a02
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-06-29 20:02:03 +0530

    Update post_software_setup.sh

--- .bin/post_software_setup.sh
+++ .bin/post_software_setup.sh
@@ -1,2 +1,11 @@
+mkdir ~/Documents/install_script_temp_folder
+cd ~/Documents/install_script_temp_folder
+
 # Bottles(Wine) Emulator
 yes | flatpak install bottles
+
+# Logisim evolution
+yay -S logisim-evolution
+
+cd ~/Documents
+rm -rf install_script_temp_folder
