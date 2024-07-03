d41e30bb640707135b9028d55bda92bf88b10d28
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-03 21:50:23 +0530

    Update post_software_setup.sh

--- .bin/post_software_setup.sh
+++ .bin/post_software_setup.sh
@@ -4,12 +4,5 @@ cd ~/Documents/install_script_temp_folder
 # Bottles(Wine) Emulator
 yes | flatpak install bottles
 
-# Logisim evolution
-yay -S logisim-evolution
-
-# KiCAD
-sudo pacman -Syu kicad
-sudo pacman -Syu --asdeps kicad-library kicad-library-3d
-
 cd ~/Documents
 rm -rf install_script_temp_folder
