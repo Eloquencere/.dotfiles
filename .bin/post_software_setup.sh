b761cee1b98db881cb85d022b3023224a1e88128
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-04 00:05:32 +0530

    Update post_software_setup.sh

--- .bin/post_software_setup.sh
+++ .bin/post_software_setup.sh
@@ -4,5 +4,9 @@ cd ~/Documents/install_script_temp_folder
 # Bottles(Wine) Emulator
 yes | flatpak install bottles
 
+# install overleaf
+yay -S yacreader
+sudo pacman -S obsidian zathura
+
 cd ~/Documents
 rm -rf install_script_temp_folder
