f8f216121248a0eccf9295293cf6cd773b5d49ce
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-03 23:50:21 +0530

    Update software_setup.sh

--- .bin/software_setup.sh
+++ .bin/software_setup.sh
@@ -63,14 +63,41 @@ yes | sudo zellij xclip
 yay -S tio
 
 # Language compilers and related packages - install these as early as possible in the script
-yes | sudo pacman -S --needed go python perl
-yes | sudo pacman -S python-pip
+sudo pacman -S gdb valgrind strace ghidra
+yes | sudo pacman -S --needed clang lldb
 yes | sudo pacman -S nodejs-lts-iron
-sudo pacman -S gdb ghidra valgrind strace
+
 # Get the appropriate profilers for all other necessary programming languages
 # yes | sudo pacman -S zig
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
 # curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh # Haskell
+
+yes | sudo pacman -S --needed perl go python 
+yes | sudo pacman -S python-pip pyenv
+pyenv install 3.12
+pyenv global 3.12
+pyenv shell 3.12
+# debugging
+pip install icecream
+# presentation
+pip install drawio
+pip install colorama pyfiglet
+pip install dash plotly seaborn
+# seaborn - (replace matplotlib)
+# data representation and calculation
+pip install mysql-connector-python
+pip install polars xarray 
+# xarray - (multi-dimentional arrays)
+pip install Cython numba taichi
+pip install numpy scipy pillow
+pip install parse
+# quality of life
+pip install pendulum
+# pendulum - (replace datetime)
+pip install pydantic
+pip install ruff
+# ruff (linter)
+
 
 # Onedriver
 # mkdir $HOME/OneDrive
