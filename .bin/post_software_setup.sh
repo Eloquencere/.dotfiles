18ad614311ebfa05e5afbc6164003f76198e70b7
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-04 08:27:11 +0530

    Update post_software_setup.sh

--- .bin/post_software_setup.sh
+++ .bin/post_software_setup.sh
@@ -1,6 +1,32 @@
 mkdir ~/Documents/install_script_temp_folder
 cd ~/Documents/install_script_temp_folder
 
+# Necessary Python libraries
+pyenv install 3.12
+pyenv global 3.12
+pyenv shell 3.12
+# debugging
+pip install icecream
+# presentation
+pip install drawio
+pip install colorama pyfiglet
+pip install dash plotly seaborn
+# seaborn - (replace matplotlib)
+# data representation and calculation
+pip install mysql-connector-python
+pip install polars xarray 
+# xarray - (multi-dimentional arrays)
+pip install Cython numba taichi
+pip install numpy scipy pillow
+pip install parse
+# quality of life
+pip install pendulum
+# pendulum - (replace datetime)
+pip install pydantic
+pip install ruff
+# ruff (linter)
+
+
 # Bottles(Wine) Emulator
 yes | flatpak install bottles
 
