3673dbce19aa6d93a12117830fe7785e32194779
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-04 18:45:52 +0530

    Create zsh-aliases.zsh

---
+++ .config/zsh/zsh-aliases.zsh
@@ -0,0 +1,9 @@
+# Aliases
+alias neofetch=fastfetch
+alias ls="eza --color=always --icons=always --git"
+alias tree="ls --tree --git-ignore"
+alias find=fd
+alias cat=bat
+alias grep=rg
+alias n=nvim
+alias df=duf
