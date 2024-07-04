e34fba26e964fa8c834782cd5da70ce5a761fa4c
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-04 21:08:11 +0530

    Update zsh-aliases.sh

--- .config/zsh/zsh-aliases.sh
+++ .config/zsh/zsh-aliases.sh
@@ -1,18 +1,11 @@
-alias neofetch=fastfetch
-alias n=nvim
+# Static
 
-# Better command line utils
-alias ls="eza --color=always --icons=always --git"
-alias tree="ls --tree --git-ignore"
-alias find=fd
-alias cat=bat
-alias grep=rg
-alias df=duf
+# Better readable format
+alias free='free -m' # show sizes in MB
 
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-# Better readable format
-alias free='free -m' # show sizes in MB
+# Variable
