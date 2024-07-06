387ae6e2fd1586552ae7c8c8268d6585f4a2a3ef
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-06 08:10:03 +0530

    Create zsh-aliases-local.sh

---
+++ local/zsh-aliases-local.sh
@@ -0,0 +1,11 @@
+# Basic tools
+alias n=nvim
+alias neofetch=fastfetch
+
+# Better command line utils
+alias ls="eza --color=always --icons=always --git"
+alias tree="ls --tree --git-ignore"
+alias find=fd
+alias cat=bat
+alias grep=rg
+alias df=duf
