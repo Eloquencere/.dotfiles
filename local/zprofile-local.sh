0d86376dd56fd6e4014148454986250ac18058d8
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-06 08:47:57 +0530

    Update zprofile-local.sh

--- local/zprofile-local.sh
+++ local/zprofile-local.sh
@@ -2,8 +2,8 @@
 export STARSHIP_CONFIG=${XDG_CONFIG_HOME}/starship/starship.toml
 
 # Pyenv
-export PYENV_ROOT="\$HOME/.pyenv"
-[[ -d PYENV_ROOT/bin ]] && export PATH="\$PYENV_ROOT/bin":$PATH
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin":$PATH
 eval "$(pyenv init -)"
 
 # Rust
@@ -24,29 +24,29 @@ source $TOOLS_HOME/Xilinx/Vitis/2024.1/settings64.sh
 # usbip
 SERVER_IP=1
 function usbip() {
-	if [[ \$1 == "attach" ]]; then
+	if [[ $1 == "attach" ]]; then
 		shift
-		sudo usbip attach --remote=\$SERVER_IP "\$@"
-	elif [[ \$1 == "detach" ]]; then
+		sudo usbip attach --remote=$SERVER_IP "$@"
+	elif [[ $1 == "detach" ]]; then
 		shift
-		sudo usbip detach "\$@"
+		sudo usbip detach "$@"
 	else
-		command usbip "\$@"
+		command usbip "$@"
 	fi
 }
 function lsusbip() {
-	IFS=$'\n' read -r -d '' -A server_devices < <(usbip list --remote=\$SERVER_IP | grep --regexp "^\s+[-0-9]+:") # might need to add local IFS=
+	IFS=$'\n' read -r -d '' -A server_devices < <(usbip list --remote=$SERVER_IP | grep --regexp "^\s+[-0-9]+:") # might need to add local IFS=
 	usbip_port_output=$(usbip port 2>/dev/null)
-	printf "Devices from %s\n" "\$SERVER_IP"
+	printf "Devices from %s\n" "$SERVER_IP"
 	printf "%-10s %-50s %-10s\n" "BUSID" "DEVICE" "PORT"
 	regex="^\s+([-0-9]+):\s+(.*)\s+(\(.*\))$"
-	for server_device in \$server_devices; do
-		if [[ \$server_device =~ \$regex ]]; then
-			busid=\$match[1]
-			device_name=\$match[2]
-			vid_pid=\$match[3]
+	for server_device in $server_devices; do
+		if [[ $server_device =~ $regex ]]; then
+			busid=$match[1]
+			device_name=$match[2]
+			vid_pid=$match[3]
 		fi
 	done
-	port_number=$(echo "\$usbip_port_output" | grep --before-context=1 "\$vid_pid" | sed --silent '1s/.*\([-0-9]\+\):.*/\1/p')
-	printf "%-10s %-50s %-10s\n" "\$busid" "\$device_name" "\$port_number"
+	port_number=$(echo "$usbip_port_output" | grep --before-context=1 "$vid_pid" | sed --silent '1s/.*\([-0-9]\+\):.*/\1/p')
+	printf "%-10s %-50s %-10s\n" "$busid" "$device_name" "$port_number"
 }
