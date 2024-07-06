effeb515547d657331983e6e82fd41eebdf20593
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-06 09:40:39 +0530

    Update zsh-aliases.sh

--- .config/zsh/zsh-aliases.sh
+++ .config/zsh/zsh-aliases.sh
@@ -1,10 +1,10 @@
-# Better readable format
-alias free='free -m' # show sizes in MB
-
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
+
+# Better readable format
+alias free='free -m' # show sizes in MB
 
 # ------- Local -------
 
