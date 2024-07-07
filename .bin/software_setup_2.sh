98d7619d7f649912c91fa1a698c25b23704af607
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-07 16:16:15 +0530

    Update software_setup_2.sh

--- .bin/software_setup_2.sh
+++ .bin/software_setup_2.sh
@@ -11,21 +11,19 @@ pip install icecream
 # presentation
 pip install drawio
 pip install colorama pyfiglet
-pip install dash plotly seaborn
-# seaborn - (replace matplotlib)
+pip install dash plotly seaborn # seaborn - (replace matplotlib)
 # data representation and calculation
 pip install mysql-connector-python
-pip install polars xarray 
-# xarray - (multi-dimentional arrays)
+pip install polars xarray # xarray - (multi-dimentional arrays)
 pip install Cython numba taichi
 pip install numpy scipy pillow
 pip install parse
 # quality of life
-pip install pendulum
-# pendulum - (replace datetime)
+pip install pendulum # replaces datetime
 pip install pydantic
-pip install ruff
-# ruff (linter)
+pip install ruff # linter
+pip install pyfiglet # Best game engine for Python
+
 
 # installing virt-manager with qemu
 yes | sudo pacman -Syu
@@ -33,8 +31,7 @@ yes | sudo pacman -S --needed archlinux-keyring
 yes | sudo pacman -S qemu-desktop virt-manager virt-viewer dnsmasq vde2 bridge-utils
 echo "n" | sudo pacman -S --needed ebtables iptables
 echo "n" | sudo pacman -S --needed libguestfs
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
+sudo systemctl enable libvirtd.service --now
 sudo usermod -a -G libvirt $(whoami)
 sudo systemctl restart libvirtd.service
 
