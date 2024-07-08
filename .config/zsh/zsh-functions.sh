e68e528d591c70a274443762c2e69cc1bf853da5
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-08 20:40:17 +0530

    Update zsh-functions.sh

--- .config/zsh/zsh-functions.sh
+++ .config/zsh/zsh-functions.sh
@@ -12,11 +12,11 @@ function usbip() {
 	fi
 }
 function lsusbip() {
-	IFS=$'\n' read -r -d '' -A server_devices < <(usbip list --remote=$SERVER_IP | grep --regexp "^\s+[-0-9]+:") # might need to add local IFS=
-	usbip_port_output=$(usbip port 2>/dev/null)
+	local IFS=$'\n' read -r -d '' -A server_devices < <(usbip list --remote=$SERVER_IP | grep --regexp "^\s+[-0-9]+:") # might need to add local IFS=
+	local usbip_port_output=$(usbip port 2>/dev/null)
 	printf "Devices from %s\n" "$SERVER_IP"
 	printf "%-10s %-50s %-10s\n" "BUSID" "DEVICE" "PORT"
-	regex="^\s+([-0-9]+):\s+(.*)\s+(\(.*\))$"
+	local regex="^\s+([-0-9]+):\s+(.*)\s+(\(.*\))$"
 	for server_device in $server_devices; do
 		if [[ $server_device =~ $regex ]]; then
 			busid=$match[1]
@@ -24,6 +24,6 @@ function lsusbip() {
 			vid_pid=$match[3]
 		fi
 	done
-	port_number=$(echo "$usbip_port_output" | grep --before-context=1 "$vid_pid" | sed --silent '1s/.*\([-0-9]\+\):.*/\1/p')
+	local port_number=$(echo "$usbip_port_output" | grep --before-context=1 "$vid_pid" | sed --silent '1s/.*\([-0-9]\+\):.*/\1/p')
 	printf "%-10s %-50s %-10s\n" "$busid" "$device_name" "$port_number"
 }
