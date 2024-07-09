1f5a80bf51629deff79d1e9bc764b78255f2c4cf
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-09 09:30:42 +0530

    Update zsh-functions.sh

--- .config/zsh/zsh-functions.sh
+++ .config/zsh/zsh-functions.sh
@@ -1,3 +1,26 @@
+# Extracting arrchives
+ex()
+{
+	if [ -f "$1" ]; then
+	case $1 in
+		*.tbz2)    tar xjf $1 ;;
+		*.tgz)     tar xzf $1 ;;
+		*.tar)     tar xf $1 ;;
+		*.tar.bz2) tar xjf $1 ;;
+		*.tar.gz)  tar xzf $1 ;;
+		*.tar.xz)  tar xf $1 ;;
+		*.tar.zst) unzstd $1 ;;
+		*.7z)      7z x $1 ;;
+		*.zip)     unzip $1 ;;
+		*.bz2)     bzip2 $1 ;;
+		*.gz)      gunzip $1 ;;
+		*.Z)       uncompress $1 ;;
+		*.rar)     unrar x $1 ;;
+		*.deb)     ar x $1 ;;
+		*)         echo "'$1' cannot be extracted via ex()" ;;
+	esac
+}
+
 # usbip
 SERVER_IP=1
 function usbip() {
