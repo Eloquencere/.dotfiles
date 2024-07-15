326e0e536ad64dccf7d82c9493f784ba69e92d72
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-15 19:37:17 +0530

    Update zsh-functions.sh

--- .config/zsh/zsh-functions.sh
+++ .config/zsh/zsh-functions.sh
@@ -36,7 +36,8 @@ usbip() {
 }
 lsusbip() {
 	local server_devices usbip_port_output
- 	local busid device_name vid_pid local
+ 	local busid device_name vid_pid port_number
+  
 	IFS=$'\n' read -r -d '' -A server_devices < <(usbip list --remote=$SERVER_IP | grep --regexp "^\s+[-0-9]+:") # might need to add local IFS=
 	usbip_port_output=$(usbip port 2>/dev/null)
 	printf "Devices from %s\n" "$SERVER_IP"
