2d8a1f19620296330ae5f02208ebd7a70ec78a64
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-07-17 21:57:57 +0530

    Update software_setup_2.sh

--- scripts/installers/software_setup_2.sh
+++ scripts/installers/software_setup_2.sh
@@ -50,36 +50,40 @@ pyenv install 3.12
 pyenv global 3.12
 source ~/.zprofile
 source ~/.config/zsh/.zshrc
-# pyenv shell 3.1
+# pyenv shell 3.12
 pip install --upgrade pip
-# debugging
-pip install icecream
-# presentation
-pip install drawio
-pip install colorama pyfiglet
-pip install dash plotly seaborn # seaborn - (replace matplotlib)
-# data representation and calculation
-pip install mysql-connector-python
-pip install polars xarray # xarray - (multi-dimentional arrays)
-pip install Cython numba taichi
-pip install numpy scipy pillow
-pip install parse
-# quality of life
-pip install pendulum # replaces datetime
-pip install pydantic
-pip install ruff # linter
-pip install mypy # static typing
-pip install pyglet # best game engine
+PIP_PKGS=(
+   # debugging
+   "icecream"
+   # presentation
+   "drawio"
+   "colorama" "pyfiglet"
+   # data representation and calculation
+   "dash plotly" "seaborn" # seaborn - (replace matplotlib)
+   "mysql-connector-python"
+   "polars" "xarray" # xarray - (multi-dimentional arrays)
+   "Cython" "numba" "taichi"
+   "numpy" "scipy" "pillow"
+   # quality of life
+   "parse"
+   "pendulum" # replaces datetime
+   "pydantic"
+   "ruff" # linter
+   "mypy" # static typing
+   "pyglet" # best game engine
+)
+pip install "${PIP_PKGS[@]}"
 
 # virt-manager with qemu/KVM
+VM_PKGS=(
+   "archlinux-keyring"
+   "qemu-desktop" "virt-manager" "virt-viewer" "dnsmasq" "vde2" "bridge-utils"
+)
 echo "do you want to install a VM software?(y/n)"
 read usr_input
 if [[ "$usr_input" == "y" ]]; then
    sudo pacman -Syu --noconfirm
-   sudo pacman -S --needed --noconfirm archlinux-keyring
-   sudo pacman -S --noconfirm qemu-desktop virt-manager virt-viewer dnsmasq vde2 bridge-utils
-   echo "n" | sudo pacman -S --needed ebtables iptables
-   echo "n" | sudo pacman -S --needed libguestfs
+   sudo pacman -S --needed --noconfirm "${VM_PKGS[@]}"
    sudo systemctl enable libvirtd.service --now
    sudo usermod -a -G libvirt $(whoami)
    sudo systemctl restart libvirtd.service
