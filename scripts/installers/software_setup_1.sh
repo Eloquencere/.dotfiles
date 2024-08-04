e5865df0b6eb46eb3ecde8b05f51b8883b4477f1
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-08-04 11:23:11 +0530

    Update software_setup_1.sh

--- scripts/installers/software_setup_1.sh
+++ scripts/installers/software_setup_1.sh
@@ -34,7 +34,7 @@ LANG_COMPILER_PKGS_PACMAN=(
   "perl" "go" "python"
   "clang" "lldb"
   "rustup"
-  "python-pip" "tk"
+  "python-pip" "tk" "python-uv"
   "ghc" "zig"
 )
 LANG_COMPILERS_PKGS_PARU=(
