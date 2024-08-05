9ea58b9654a716f2fbb363118d8c3525cfb03677
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-08-05 22:42:50 +0530

    Update zsh-aliases.zsh

--- .config/zsh/zsh-aliases.zsh
+++ .config/zsh/zsh-aliases.zsh
@@ -14,7 +14,7 @@ alias py=python
 # Better command line utils
 alias ls="eza --color=always --icons=always --git"
 alias tree="ls --tree --git-ignore"
-alias find=fd
+alias find="fd --color=always"
 alias cat=bat
 alias grep=rg
 alias df=duf
