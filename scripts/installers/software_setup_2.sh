ab853d71ec9f91d6fd20b72d9168581010de2caf
Eloquencere <sriranga.jaguar@gmail.com>
2024-09-05 15:00:33 +0530

    better way of accessing the cloud

--- scripts/installers/software_setup_2.sh
+++ scripts/installers/software_setup_2.sh
@@ -65,19 +65,7 @@ if [[ $usr_input =~ ^[Yy]$ ]]; then
     sed -i '/.* = $/d' $HOME/.gitconfig
 fi
 
-echo -n "Would you like to setup your cloud file storage client?(Y/n)"
-read usr_input
-if [[ $usr_input =~ ^[Yy]$ ]]; then
-	rm -rf ~/Pictures ~/Templates ~/Public ~/Videos
-	echo "Open settings/Online Accounts and log into your provider"
-	echo -n "Would you like to sym-link the cloud directory to your home directory?(Y/n)"
-	read usr_input
-	if [[ $usr_input =~ ^[Yy]$ ]]; then
-		echo -n "Enter the absolute path to the cloud directory: "
-		read path
-		ln -s $path $HOME
-	fi
-fi
+# rm -rf ~/Pictures ~/Templates ~/Public ~/Videos
 
 mkdir ~/Documents/install_script_temp_folder
 cd ~/Documents/install_script_temp_folder
