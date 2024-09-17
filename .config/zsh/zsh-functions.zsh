407007b3221deeff7b3feef30ad791e9dbe6605b
Eloquencere <sriranga.jaguar@gmail.com>
2024-09-17 21:31:13 +0530

    improvements to croc

--- .config/zsh/zsh-functions.zsh
+++ .config/zsh/zsh-functions.zsh
@@ -4,14 +4,32 @@ croc() {
 		if [[ $1 == "--here" ]]; then
 			shift
 			export CROC_SECRET=$(sqlite3 $ZDOTDIR/.confidential/croc_collaborators_registry.db "SELECT Transfer_Code FROM collaborator_catalogue WHERE ID='$1';")
-			command croc && echo "\033[33mTransfer received\033[0m in current working directory"
+			command croc
+            echo "\033[33mTransfer received\033[0m in current working directory"
 		else
 			export CROC_SECRET=$(sqlite3 $ZDOTDIR/.confidential/croc_collaborators_registry.db "SELECT Transfer_Code FROM collaborator_catalogue WHERE ID='$1';")
-			command croc --out $HOME/croc-inbox && echo "\033[32mTransfer received\033[0m in ~/croc-inbox"
+			command croc --out $HOME/croc-inbox 
+            echo "\033[32mTransfer received\033[0m in ~/croc-inbox"
 		fi
 	elif [[ $1 == "send" ]]; then
+        shift
+        if [[ $1 == "--help" || $1 == "-h" ]]; then
+            command croc send --help
+            return
+        elif [[ $1 =~ "--delete" ]]; then
+            shift
+            local ask_user_to_delete=1
+        fi
 		export CROC_SECRET=$(sqlite3 $ZDOTDIR/.confidential/croc_collaborators_registry.db "SELECT Transfer_Code FROM collaborator_catalogue WHERE Self=1;")
-        command croc "$@"
+        command croc send "$@"
+        if [[ $? -eq 1 || $ask_user_to_delete -ne 1 ]]; then
+            return
+        fi
+        echo -n "\033[31mDelete the following\033[0m transfered data?\n $@ \n(y/N)"
+        read delete_confirmation
+        if [[ $delete_confirmation =~ ^[Yy]$ ]]; then
+            rm -rf "$@"
+        fi
     else
         command croc "$@"
 	fi
