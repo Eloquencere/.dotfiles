d7508c5c753b61bd79b6cdab4eb61870b78c03e8
Eloquencere <sriranga.jaguar@gmail.com>
2024-09-18 23:21:10 +0530

    greatly improved croc's tab completion

--- .config/zsh/completion/_croc.zsh
+++ .config/zsh/completion/_croc.zsh
@@ -3,20 +3,51 @@
 _croc() {
 	local line state
 
-	_arguments -C \
-		"1: :->cmds" \
-		"*::arg:->args"
+	_arguments -s -S \
+        '(--internal-dns)--internal-dns[use a built-in DNS stub resolver rather than the host operating system]' \
+        '(--classic)--classic[toggle between the classic mode (insecure due to local attack vector) and new mode (secure)]' \
+        '(--remember)--remember[save these settings to reuse next time]' \
+        '(--debug)--debug[toggle debug mode]' \
+        '(--yes)--yes[automatically agree to all prompts]' \
+        '(--stdout)--stdout[redirect file to stdout]' \
+        '(--no-compress)--no-compress[disable compression]' \
+        '(--ask)--ask[make sure sender and recipient are prompted]' \
+        '(--local)--local[force to use only local connections]' \
+        '(--ignore-stdin)--ignore-stdin[ignore piped stdin]' \
+        '(--overwrite)--overwrite[do not prompt to overwrite or resume]' \
+        '(--testing)--testing[flag for testing purposes]' \
+        '(--curve)--curve value[choose an encryption curve(p521, p256, p384, siec)]' \
+        '(--ip)--ip value[set sender ip if known e.g. 10.0.0.1:9009, [::1]:9009]' \
+        '(--relay)--relay value[IPv4 address of the relay]' \
+        '(--relay6)--relay6 value[ipv6 address of the relay]' \
+        '(--out)--out value[specify an output folder to receive the file]' \
+        '(--pass)--pass value[password for the relay]' \
+        '(--socks5)--socks5 value[add a socks5 proxy]' \
+        '(--connect)--connect[add a http proxy]' \
+        '(--throttleUpload)--throttleUpload[Throttle the upload speed e.g. 500k]' \
+        '(-h --help)'{-h,--help}'[show help]' \
+        '(-v --version)'{-v,--version}'[print the version]' \
+        '1: :->cmds' \
+        "*::arg:->args"
 
 	case "$state" in
 		cmds)
 			_values "croc command" \
-				"send[send local files to a receiver]" \
-				"recv[receive a transfer from a sender]"
+				"send[send file(s), or folder (see options with croc send -h)]" \
+                "relay[start your own relay (optional)]" \
+                "help[Shows a list of commands or help for one command]" \
+				"recv[receive a transfer from a sender]" \
 			;;
 		args)
 			case $line[1] in
 				send)
 					_croc_send_cmd
+				;;
+				relay)
+					_croc_relay_cmd
+				;;
+				help)
+					_croc_help_cmd
 				;;
 				recv)
 					_croc_recv_cmd
@@ -26,17 +57,83 @@ _croc() {
 	esac
 }
 
-_croc_recv_cmd() {
-	local line state
+_croc_send_cmd() {
+    local state line curcontext=$curcontext ret=1
 
+	_arguments -s -S \
+        '(--zip)--zip[zip folder before sending]' \
+        '(-c --code)'{-c,--code}'[codephrase used to connect to relay]' \
+        '(--hash)--hash[hash algorithm (xxhash, imohash, md5)]' \
+        '(-t --text)'{-t,--text}'[send some text]' \
+        '(--no-local)--no-local[disable local relay when sending]' \
+        '(--no-multi)--no-multi[disable multiplexing]' \
+        "(--git)--git[enable .gitignore respect \/ don\'t send ignored files]" \
+        '(--port)--port[base port for the relay]' \
+        '(--transfers)--transfers[number of ports to use for transfers]' \
+        '(--delete)--delete[delete all the files after transfer is complete]' \
+        '(-h --help)'{-h,--help}'[show help]' \
+        '*:: :->file'
+
+	case "$state" in
+          (file)
+                (( CURRENT > 0 )) && line[CURRENT]=()
+                line=( ${line//(#m)[\[\]()\\*?#<>~\^\|]/\\$MATCH} )
+                _files -F line && ret=0
+          ;;
+	esac
+}
+
+_croc_relay_cmd() {
+    local line state
+
+	_arguments -s -S \
+        '(--host)--host[host of the relay]' \
+        '(--ports)--ports[ports of the relay]' \
+        '(--port)--port[base port for the relay]' \
+        '(--transfers)--transfers[number of ports to use for relay]' \
+        '(-h --help)'{-h,--help}'[show help]' \
+        '1: :->cmds'
+        "*::arg:->args"
+
+    case "$state" in
+        cmds)
+            _values "croc command" \
+                "send[send file(s), or folder (see options with croc send -h)]" \
+                "help[Shows a list of commands or help for one command]" \
+                "recv[receive a transfer from a sender]" \
+            ;;   
+        args)
+			case $line[1] in
+				send)
+					_croc_send_cmd
+				;;
+				help)
+					_croc_help_cmd
+				;;
+				recv)
+					_croc_recv_cmd
+				;;
+			esac
+			;;
+    esac
+}
+
+_croc_help_cmd() {
+    _arguments -C \
+        '(-h --help)'{-h,--help}'[show help]' \
+}
+
+_croc_recv_cmd() {
 	declare -a local_list
 	while IFS='|' read -r Name ID Designation; do
 		local_list+=("${ID}[${Designation} > ($Name)]")
 	done <<< $(sqlite3 $ZDOTDIR/.confidential/croc_collaborators_registry.db "SELECT Name,ID,Designation FROM collaborator_catalogue WHERE ID!='$CROC_SELF_TRANSFER_ID';")
 
+	local line state
+
 	_arguments -C \
-		"1: :->cmds" \
-		"*::arg:->args"
+        '(--here)--here[receive the files here]' \
+		"1: :->cmds"
 	
 	case "$state" in
 		cmds)
@@ -44,11 +141,6 @@ _croc_recv_cmd() {
 				${local_list[@]}
 			;;
 	esac
-}
-
-_croc_send_cmd() {
-	_path_files
-	# add --zip & --text & --git
 }
 
 compdef _croc croc
