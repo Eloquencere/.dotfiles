3be7459b9d32522220956e52515b222af62cd823
Eloquencere <sriranga.jaguar@gmail.com>
2025-10-20 10:02:54 -0400

    updates

---
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,18 @@
+return {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    event = 'VeryLazy',
+    lazy = true,
+    keys = {
+        { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+        { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+    opts = {
+        save_path = "~/Pictures",
+        has_breadcrumbs = true,
+        bg_padding = 0,
+        bg_theme = "bamboo",
+        watermark = "",
+        has_line_number = true,
+    },
+}
