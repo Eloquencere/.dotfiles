896a41b6da13aa94dc2b30cf9428b21ccb9dd9ca
Eloquencere <sriranga.jaguar@gmail.com>
2026-02-03 16:03:17 -0500

    added tmux config just for legacy purposes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -41,6 +41,7 @@ config = {
     font_size = 17.5,
     use_ime = false,
     default_cursor_style = "SteadyBar",
+    enable_osc52_copy = true, -- For remote connections
 
     -- Key mappings for tab movement in neovim, thanks to - reddit.com/r/neovim/comments/uc6q8h/ability_to_map_ctrl_tab_and_more/
     keys = {
@@ -59,6 +60,8 @@ config = {
             mods = "SHIFT",
             action = wezterm.action.SendString("\x1b[13;2u"),
         },
+
+        -- To Address Zellij issue
         {
             key = "Enter",
             mods = "SHIFT",
