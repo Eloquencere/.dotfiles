6c3d272f1dd4a29f0e0cdf8701f1e7dee646bb80
Eloquencere <sriranga.jaguar@gmail.com>
2026-09-17 08:00:08 -0400

    gh test

--- ubuntu-postinstall-scripts/software-installer/installer_2.zsh
+++ ubuntu-postinstall-scripts/software-installer/installer_2.zsh
@@ -110,9 +110,9 @@ if [[ $user_choice =~ ^[Yy]$ ]]; then
     read -r "email?Email ID: "
     git config --global user.email "$email"
     echo "you need to login to Github as well"
-    gh auth login
-    # WARN: include logic to give read & write access to projects
-    sed -i '/.* = $/d' $XDG_CONFIG_HOME/git/config
+    gh auth login     # WARN: include logic to give read & write access to projects
+    git config --global --replace-all 'credential.https://github.com.helper' '!'"$(which gh) auth git-credential"
+    git config --global --replace-all 'credential.https://gist.github.com.helper' '!'"$(which gh) auth git-credential"
 fi
 
 # Speech to Text
